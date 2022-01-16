e5df26b0cdf05d85d5c18db03b13c364aebe69d0
Federico Galatolo <galatolo.federico@gmail.com>
2022-01-16 17:23:51 +0100

    parametric generate scenario script

--- scripts/generate-scenario-italy.py
+++ scripts/generate-scenario-italy.py
@@ -10,25 +10,34 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument("--dataset", type=str, default="./dataset/italy.xlsx")
 parser.add_argument("--output", type=str, default="scenarios/italy.yaml")
-parser.add_argument("--quantity-std", type=float, default=0)
+parser.add_argument("--quantity-mu-formula", type=str, default="{zone_machines_count}")
+parser.add_argument("--quantity-std-formula", type=str, default="{zone_machines_count}")
+parser.add_argument("--probability-formula", type=str, default="2/100")
+parser.add_argument("--duration-mu", type=float, default=60)
+parser.add_argument("--duration-std", type=float, default=5)
+parser.add_argument("--max-delivery-delta-formula", type=str, default="24*5+{duration}")
+
+parser.add_argument("--same-zone-logistic-cost", type=float, default=12)
+parser.add_argument("--global-zone-logistic-cost", type=float, default=36)
+
 
 args = parser.parse_args()
 
 dataset = pd.read_excel(args.dataset)
 
-zone_nodes_count = dict()
-zone_machines_count = dict()
+zones_nodes_count = dict()
+zones_machines_count = dict()
 node_machines_count = dict()
 zones = []
 nodes = []
 demands = []
 for idx, elem in dataset.iterrows():
     region = elem["Region"]
-    if region not in zone_nodes_count: zone_nodes_count[region] = 0
-    zone_nodes_count[region] += 1
-    node_name = f"{region}-{zone_nodes_count[region]}"
+    if region not in zones_nodes_count: zones_nodes_count[region] = 0
+    zones_nodes_count[region] += 1
+    node_name = f"{region}-{zones_nodes_count[region]}"
     zone_name = f"{region}Zone"
-    if zone_name not in zone_machines_count: zone_machines_count[zone_name] = 0
+    if zone_name not in zones_machines_count: zones_machines_count[zone_name] = 0
 
     zone = None
     for z in zones:
@@ -43,7 +52,7 @@ for idx, elem in dataset.iterrows():
         zones.append(zone)
     
     zone["nodes"].append(node_name)
-    zone_machines_count[zone_name] += elem["Quantity"]
+    zones_machines_count[zone_name] += elem["Quantity"]
     node_machines_count[node_name] = elem["Quantity"]
     
 global_zone = dict(
@@ -55,6 +64,8 @@ for zone in zones:
     global_zone["nodes"].extend(zone["nodes"])
     global_zone["customers"].extend(zone["customers"])
     
+    zone_machines_count = zones_machines_count[zone["name"]]
+
     demand = dict(
         type="ZoneDemand",
         name=f"{zone['name']}Demand",
@@ -62,23 +73,23 @@ for zone in zones:
         quantity=dict(
             type="normal",
             cast="positive_integer",
-            mu=atleast_1(zone_machines_count[zone["name"]]),
-            std=args.quantity_std
+            mu=atleast_1(eval(args.quantity_mu_formula.format(zone_machines_count=zone_machines_count))),
+            std=atleast_1(eval(args.quantity_std_formula.format(zone_machines_count=zone_machines_count)))
         ),
         probability=dict(
             type="formula",
-            value="1/100"
+            value=args.probability_formula
         ),
         duration=dict(
             type="normal",
             cast="positive_integer",
-            mu=60,
-            std=5,
+            mu=args.duration_mu,
+            std=args.duration_std,
         ),
         max_delivery_delta=dict(
             type="formula",
             cast="positive_integer",
-            value="24*5+{duration}"
+            value=args.max_delivery_delta_formula
         )
     )
     demands.append(demand)
@@ -99,14 +110,14 @@ for zone in zones:
                     zone=zone["name"],
                     cost=dict(
                         type="constant",
-                        value=12
+                        value=args.same_zone_logistic_cost
                     )
                 ),
                 dict(
                     zone="GlobalZone",
                     cost=dict(
                         type="constant",
-                        value=36
+                        value=args.global_zone_logistic_cost
                     )
                 )
             ]
